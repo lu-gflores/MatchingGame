938530e61cb47fc704db8c313615f080d485cfa2
George Flores <georgeflores886@gmail.com>
2020-08-03 12:42:41 -0400

    added chained methods

--- src/Board.js
+++ src/Board.js
@@ -7,19 +7,19 @@ export default class Board {
         this.grid = [];
 
         this.reserverGrid = [];
-        for(let i = 0; i < rows; i++) {
+        for (let i = 0; i < rows; i++) {
             this.grid.push([])
             this.reserverGrid.push([])
         }
-        
+
         this.populateGrid();
         this.populateReserveGrid();
         this.consoleLog()
-    }  
+    }
 
-    populateGrid () {
-        for(let i = 0; i < this.rows; i++) {
-            for(let j = 0; j < this.cols; j++) {
+    populateGrid() {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
                 const variation = Math.floor(Math.random() * this.blockVariations) + 1
                 this.grid[i].push(variation)
             }
@@ -27,37 +27,37 @@ export default class Board {
     }
 
     populateReserveGrid() {
-        for(let i = 0; i < this.rows; i++) {
-            for(let j = 0; j < this.cols; j++) {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
                 const variation = Math.floor(Math.random() * this.blockVariations) + 1
                 this.reserverGrid[i].push(variation)
             }
         }
     }
-    consoleLog () {
+    consoleLog() {
         if (this.debug) {
 
             let prettyString = '';
-            for(let i = 0; i < this.rows; i++) {
+            for (let i = 0; i < this.rows; i++) {
                 prettyString += '\n';
-                for(let j = 0; j < this.cols; j++) {
-                   prettyString += ' ' + this.reserverGrid[i][j]
+                for (let j = 0; j < this.cols; j++) {
+                    prettyString += ' ' + this.reserverGrid[i][j]
                 }
             }
             //seperate grids
             prettyString += '\n'
-            for(let j = 0; j < this.cols; j++) {
+            for (let j = 0; j < this.cols; j++) {
                 prettyString += ' -';
-            } 
+            }
             //print main grid
-            for(let i = 0; i < this.rows; i++) {
+            for (let i = 0; i < this.rows; i++) {
                 prettyString += '\n';
-                for(let j = 0; j < this.cols; j++) {
-                   prettyString += ' ' + this.grid[i][j]
+                for (let j = 0; j < this.cols; j++) {
+                    prettyString += ' ' + this.grid[i][j]
                 }
             }
             console.log(prettyString);
-      
+
         }
 
     }
@@ -80,5 +80,65 @@ export default class Board {
         const diffCol = Math.abs(source.col - target.col);
         const isAdjacent = (diffRow === 1 && diffCol === 0) || (diffRow === 0 && diffCol === 1)
         return isAdjacent
+    }
+    isChained(block) {
+        let isChained = false;
+        const variation = this.grid[block.row][block.col]
+        const { row, col } = block;
+        //left
+        if (variation === this.grid[row][col - 1] && variation === this.grid[row][col - 2]) {
+            isChained = true;
+        }
+        //right
+        if (variation === this.grid[row][col + 1] && variation === this.grid[row][col + 2]) {
+            isChained = true;
+        }
+        //up
+        if (this.grid[row - 2]) {
+            if (variation === this.grid[row - 1][col] && variation === this.grid[row - 2][col]) {
+                isChained = true;
+            }
+        }
+        //down
+        if (this.grid[row + 2]) {
+            if (variation === this.grid[row + 1][col] && variation === this.grid[row + 2][col]) {
+                isChained = true;
+            }
+        }
+        //center horizontal
+        if (variation === this.grid[row][col - 1] && variation === this.grid[row][col + 1]) {
+            isChained = true;
+        }
+        //center vertical
+        if (this.grid[row + 1] && this.grid[row - 1]) {
+            if (variation === this.grid[row + 1][col] && variation === this.grid[row - 1][col]) {
+                isChained = true;
+            }
+        }
+        return isChained
+    }
+    findAllChains() {
+        const chained = [];
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                if (this.isChained({ row: i, col: j })) {
+                    chained.push({ row: i, col: j })
+                }
+            }
+        }
+        return chained;
+    }
+    clearChains() {
+        //get all blocks that need to be cleared
+        const chainedBlocks = this.findAllChains();
+        chainedBlocks.forEach((block) => {
+            //reset to 0
+            this.grid[block.row][block.col] = 0;
+    
+            //destroy block
+
+
+        })
+        this.consoleLog();
     }
 }
