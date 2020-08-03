437a4fbadcb586abeca762cde6a16ee09600035f
George Flores <georgeflores886@gmail.com>
2020-08-03 13:10:52 -0400

    added kontra load method

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,6 @@
 import Grid from './Grid.js'
 import Board from './Board.js'
-const { init, GameLoop, Sprite, initPointer, track } = kontra;
+const { init, GameLoop, Sprite, initPointer, track, load, on } = kontra;
 
 
 export default class Game {
@@ -38,7 +38,29 @@ export default class Game {
     load() {
         console.log('loading assets')
 
-        this.start();
+        on('assetLoaded', (asset, url) => {
+            asset.id = url;
+        })
+        //promised based asset loader that will return promise
+        // that resolves when all assets are loaded
+        load(
+            'assets/images/bean_blue.png',
+            'assets/images/bean_green.png',
+            'assets/images/bean_orange.png',
+            'assets/images/bean_pink.png',
+            'assets/images/bean_purple.png',
+            'assets/images/bean_red.png',
+            'assets/images/bean_yellow.png',
+            'assets/images/bean_white.png',
+            'assets/images/bean_dead.png',
+        ).then((assets) => {
+            this.assets = assets;
+            this.start();
+            console.log(this.assets)
+        }).catch((error) => {
+            console.log(error)
+        })
+        
     }
     start() {
         console.log('starting game')
