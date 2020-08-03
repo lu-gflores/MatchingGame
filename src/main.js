54327e9013fa7211c5256a0422445b46ce7900be
George Flores <georgeflores886@gmail.com>
2020-08-03 11:00:24 -0400

    added resize.js for game

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Game from './Game.js';
-
+import {resize} from './resize.js'
 const HEIGHT =640
 const WIDTH = 360
 
 const game = new Game(WIDTH, HEIGHT);
 console.log(game)
+
+resize(WIDTH, HEIGHT)
+window.addEventListener('resize', () => {
+    resize(WIDTH, HEIGHT)
+}, false)
